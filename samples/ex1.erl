42fecd32944c3f5b3aed236b613b42d1012faa2c
komone <steven.charles.davis@gmail.com>
2009-03-09 07:55:18 -0500

    Updated ex1 for both term or xml ui defs

--- samples/ex1.erl
+++ samples/ex1.erl
@@ -3,19 +3,27 @@
 
 %% GS Reference: http://www.erlang.org/doc/apps/gs/gs_chapter2.html#2.2
 
--export([init/0, on_message/1, on_close/0]).
+-export([init/0, init/1, on_message/1, on_close/0]).
 
 %% Experimental!!
-%% e.g. callback names are currently hard coded in gx...
 init() -> 
 	UI = 
 	[{
-		window, [{width, 200}, {height, 100}], [{
+		window, [{width, 200}, {height, 100}], 
+		[{ 
 			button, [{label, "Press Me"}], []
 		}]
 	}],
 	gx:start(?MODULE, UI).
-	
+
+%% or use ex1.xml
+init(File) ->
+	gx:start(?MODULE, File).
+
+%
+% Callbacks (currently required)
+% names are currently hard coded in gx...
+%
 on_message(_Message) ->
 	io:format("Hello There~n", []).
 
